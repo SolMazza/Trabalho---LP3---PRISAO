47b1a0cd5b45ee99c0043c3941fb5d75d1401159
Sol Mazza <145726469+SolMazza@users.noreply.github.com>
2024-10-08 19:41:17 -0300

    Delete Presidio-tab2.FINAL directory

--- Presidio-tab2.FINAL/src/main/java/org/example/exception/NoneCellFind.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class NoneCellFind extends RuntimeException {
-    public NoneCellFind(String message) {
-        super(message);
-    }
-}
--- Presidio-tab2.FINAL/src/main/java/org/example/exception/NonePrisoner.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class NonePrisoner extends RuntimeException {
-    public NonePrisoner(String message) {
-        super(message);
-    }
-}
